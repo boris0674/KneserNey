568fbcf7184cad038128dd0aff548c6e5dd4d5d9
boris secciani <33371915+boris0674@users.noreply.github.com>
2019-06-30 11:22:11 +0200

    Add files via upload

---
+++ global.R
@@ -0,0 +1,26 @@
+library(tm)
+
+library(dplyr)
+library(quanteda)
+library(readr)
+library(ngram)
+library(tidyr)
+
+library(tidytext)
+library(shinythemes)
+
+library(shiny)
+
+sepone <- readRDS("sep_one.rds")
+
+sepbi <- readRDS("sep_bi.rds")
+
+septri <- readRDS("sep_tri.rds")
+
+
+sepfour <- readRDS("sep_four.rds")
+
+sepfive <- readRDS("sep_five.rds")
+
+
+
